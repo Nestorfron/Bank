0077af641ab926a9e9e6c997edca64477e15d1ed
Nestorfron <nestorfrones07@hotmail.com>
2024-10-16 10:16:56 -0300

    map

--- src/front/js/pages/dashboard.jsx
+++ src/front/js/pages/dashboard.jsx
@@ -12,6 +12,7 @@ import {
   Image,
   Button,
 } from "@nextui-org/react";
+import Map from "../component/Map.jsx";
 
 const Dashboard = () => {
   const { store, actions } = useContext(Context);
@@ -32,20 +33,8 @@ const Dashboard = () => {
   return (
     <div className="gap-2 grid grid-cols-12 grid-rows-2 px-8 mt-5 mb-5">
       <Card className="col-span-12 sm:col-span-4 h-[300px]">
-        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-          <p className="text-tiny text-white/60 uppercase font-bold">
-            What to watch
-          </p>
-          <h4 className="text-white font-medium text-large">
-            Stream the Acme event
-          </h4>
-        </CardHeader>
-        <Image
-          removeWrapper
-          alt="Card background"
-          className="z-0 w-full h-full object-cover"
-          src="https://nextui.org/images/card-example-4.jpeg"
-        />
+        {" "}
+        <Map />
       </Card>
       <Card className="col-span-12 sm:col-span-4 h-[300px]">
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
